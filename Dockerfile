37bf07e2750f4b4caa7046206ac2d6b5695b72f4
Justan Other Human <iamjustanotherhuman@gmail.com>
2019-10-22 23:38:35 -0500

    added docker compose file and redis

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,13 @@
 # use an existing docker image as a base
 FROM node:alpine
 
+WORKDIR /app
+
 # download and install a dependency
 #RUN apk add --update redis
+COPY package.json .
 RUN npm install
-
+COPY . .
 
 # Tell the image what to do when it starts as a container
 #CMD ["redis-server"]
--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const redis = require('redis');
+const process = require('process')
 
 const app = express();
+const client = redis.createClient({
+  host: 'redis-server',
+  port: 6379 //default port number for redis
+});
+client.set('visits', 0);
 
 app.get('/', (req, res) => {
-  res.send('IBM is awesome!')
+  process.exit(0); //exit status code of 0, everything is OK 
+  client.get('visits', (err, visits) => {
+    res.send('IBM is awesome!, Number of visits is ' + visits);
+    client.set('visits', parseInt(visits) + 1);
+  });
 });
 
 app.listen(8080, () => {
