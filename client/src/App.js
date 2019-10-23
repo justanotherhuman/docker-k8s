b755ccda36586c2898a60869282fb41c714e49d2
Justan Other Human <iamjustanotherhuman@gmail.com>
2019-10-23 08:18:21 -0500

    onwards to nginx

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>IBM and NYC, here I come</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Garage Cloud, go big blue
+        </a>
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
